200d863486fb70fd9cc4286572a77884434ee49f
noorul-wahab <noorjugni@yahoo.com>
2023-09-14 14:15:41 +0100

    updated survival script

--- Forest_plots/forest_plot_R_script.R
+++ Forest_plots/forest_plot_R_script.R
@@ -10,7 +10,7 @@ library("survminer")
 forest_plot <- function(work_dir, csv_name, feat, LN_status, event_name, censor_at, plot_width, plot_height, cohort_name){
   setwd(work_dir)
   tdata <- read.csv(file=csv_name, header=TRUE, sep=",", check.names=TRUE)
-
+  
   ##drop cases with LN=+  ##Lymph.Node.status
   if (LN_status == 0)
     tdata <- tdata[!(tdata$Lymph.Node.status==1),]
@@ -22,11 +22,11 @@ forest_plot <- function(work_dir, csv_name, feat, LN_status, event_name, censor_
   tdata$BRACE[tdata$BRACE1>1.28]<-"1"
   
   tdata$Magee2_no_npi = tdata$Magee_new2_no_npi
-  if (cohort_name=="NOTT"){
+  if (cohort_name=="Cohort-A"){
     tdata$Magee2_nonpi[tdata$Magee2_no_npi<=11]<-"0"
     tdata$Magee2_nonpi[tdata$Magee2_no_npi>11]<-"1"}
   
-  tdata$Age2 = tdata$Age
+  tdata$Age2 = tdata$Age.at.Diagnosis
   tdata$Age[tdata$Age2>=50]<-"1"
   tdata$Age[tdata$Age2<50]<-"0"
   
@@ -43,7 +43,7 @@ forest_plot <- function(work_dir, csv_name, feat, LN_status, event_name, censor_
     tdata$time = tdata$Breast.cancer.specific.survival..month  ###@@@@@@@@ for DMFS: TTDM..month, for BCSS: Breast.cancer.specific.survival..month  @@@@@@@@@@@@@@@@@
     tdata$event = tdata$Survival.Status ###@@@@@@@ for DMFS: Distant.Metastasis, for BCSS: Survival.Status  @@@@@@@@@@@@@@@@
   }
-
+  
   ##censor the data  
   tdata$event[tdata$time > censor_at] <- 0  
   tdata$time[tdata$time > censor_at] <- censor_at
@@ -52,56 +52,56 @@ forest_plot <- function(work_dir, csv_name, feat, LN_status, event_name, censor_
   tdata$event[tdata$event > 1] <- 0
   
   if (feat == "BRACE"){
-    if(cohort_name == 'NOTT'){
+    if(cohort_name == 'Cohort-A'){
       if (LN_status == 0){
         tdata <- tdata %>%
-        transmute(time, event, 
-                'Age at diagnosis' = factor(Age, labels = c("<50",">=50")),
-                'Multifocality' = factor(Multifocality, labels = c("No","Yes")),
-                'DCIS' = factor(Associated_DCIS, labels = c("No","Yes")),
-                'LCIS' = factor(Associated_LCIS, labels = c("No","Yes")),
-                LVI = factor(LVI, labels = c("No","Yes")),
-                Grade = factor(Grade, labels = c("1, 2","3")),
-                Magee2 = factor(Magee2_nonpi, labels = c("Low","High")),
-                BRACE = factor(BRACE, labels = c("Low","High")),)
+          transmute(time, event, 
+                    'Age at diagnosis' = factor(Age, labels = c("<50",">=50")),
+                    'Multifocality' = factor(Multifocality, labels = c("No","Yes")),
+                    'DCIS' = factor(Associated_DCIS, labels = c("No","Yes")),
+                    'LCIS' = factor(Associated_LCIS, labels = c("No","Yes")),
+                    LVI = factor(LVI, labels = c("No","Yes")),
+                    Grade = factor(Grade, labels = c("1, 2","3")),
+                    Magee2 = factor(Magee2_nonpi, labels = c("Low","High")),
+                    BRACE = factor(BRACE, labels = c("Low","High")),)
       }
       if (LN_status == 1){
         tdata <- tdata %>%
           transmute(time, event, 
-                'Age at diagnosis' = factor(Age, labels = c("<50",">=50")),
-                'Multifocality' = factor(Multifocality, labels = c("No","Yes")),
-                'DCIS' = factor(Associated_DCIS, labels = c("No","Yes")),
-                'LCIS' = factor(Associated_LCIS, labels = c("No","Yes")),
-                'Lymph node status' = factor(Lymph.Node.status, labels = c("No","Yes")),
-                LVI = factor(LVI, labels = c("No","Yes")),
-                Grade = factor(Grade, labels = c("1, 2","3")),
-                Magee2 = factor(Magee2_nonpi, labels = c("Low","High")),
-                BRACE = factor(BRACE, labels = c("Low","High")),)
+                    'Age at diagnosis' = factor(Age, labels = c("<50",">=50")),
+                    'Multifocality' = factor(Multifocality, labels = c("No","Yes")),
+                    'DCIS' = factor(Associated_DCIS, labels = c("No","Yes")),
+                    'LCIS' = factor(Associated_LCIS, labels = c("No","Yes")),
+                    'Lymph node status' = factor(Lymph.Node.status, labels = c("No","Yes")),
+                    LVI = factor(LVI, labels = c("No","Yes")),
+                    Grade = factor(Grade, labels = c("1, 2","3")),
+                    Magee2 = factor(Magee2_nonpi, labels = c("Low","High")),
+                    BRACE = factor(BRACE, labels = c("Low","High")),)
       }
     }
-    if(cohort_name == 'UHCW'){
+    if(cohort_name == 'Cohort-B'){
       if (LN_status == 0){
         tdata <- tdata %>%
           transmute(time, event, 
-                'Age at diagnosis' = factor(Age, labels = c("<50",">=50")),
-                'Multifocality' = factor(Multifocality, labels = c("No","Yes")),
-                'DCIS' = factor(Associated_DCIS, labels = c("No","Yes")),
-                'LCIS' = factor(Associated_LCIS, labels = c("No","Yes")),
-                'Tumour size' = factor(Tumour_Size, labels = c("<2 cm",">=2 cm")),
-                Grade = factor(Grade, labels = c("1, 2","3")),
-                BRACE = factor(BRACE, labels = c("Low","High")),)
+                    'Age at diagnosis' = factor(Age, labels = c("<50",">=50")),
+                    'Multifocality' = factor(Multifocality, labels = c("No","Yes")),
+                    'DCIS' = factor(Associated_DCIS, labels = c("No","Yes")),
+                    'LCIS' = factor(Associated_LCIS, labels = c("No","Yes")),
+                    'Tumour size' = factor(Tumour_Size, labels = c("<2 cm",">=2 cm")),
+                    Grade = factor(Grade, labels = c("1, 2","3")),
+                    BRACE = factor(BRACE, labels = c("Low","High")),)
       }
       if (LN_status == 1){
         tdata <- tdata %>%
           transmute(time, event, 
-                'Age at diagnosis' = factor(Age, labels = c("<50",">=50")),
-                'Multifocality' = factor(Multifocality, labels = c("No","Yes")),
-                'DCIS' = factor(Associated_DCIS, labels = c("No","Yes")),
-                'LCIS' = factor(Associated_LCIS, labels = c("No","Yes")),
-                'Tumour size' = factor(Tumour_Size, labels = c("<2 cm",">=2 cm")),
-                'Lymph node status' = factor(Lymph.Node.status, labels = c("No","Yes")),
-                Grade = factor(Grade, labels = c("1, 2","3")),
-                BRACE = factor(BRACE, labels = c("Low","High")),)
+                    'Age at diagnosis' = factor(Age, labels = c("<50",">=50")),
+                    'Multifocality' = factor(Multifocality, labels = c("No","Yes")),
+                    'DCIS' = factor(Associated_DCIS, labels = c("No","Yes")),
+                    'LCIS' = factor(Associated_LCIS, labels = c("No","Yes")),
+                    'Tumour size' = factor(Tumour_Size, labels = c("<2 cm",">=2 cm")),
+                    'Lymph node status' = factor(Lymph.Node.status, labels = c("No","Yes")),
+                    Grade = factor(Grade, labels = c("1, 2","3")),
+                    BRACE = factor(BRACE, labels = c("Low","High")),)
       }
     }
   }
@@ -110,7 +110,7 @@ forest_plot <- function(work_dir, csv_name, feat, LN_status, event_name, censor_
     list(width = 0.01),
     list(width = 0.1, display = ~variable, fontface = "bold", heading = "Variable"),
     list(width = 0.1, display = ~level, heading = "Strata"),
-    list(width = 0.05, display = ~n, hjust = 1, heading = "N"),
+    list(width = 0.05, display = ~n, hjust = 1, heading = "n"),
     
     list(width = 0.03, item = "vline", hjust = 0.5),
     list(width = 0.55, item = "forest", hjust = 0.5, heading = "", linetype = "dashed", line_x = 0),
@@ -126,32 +126,29 @@ forest_plot <- function(work_dir, csv_name, feat, LN_status, event_name, censor_
     ),
     list(width = 0.03)
   )
-
+  
   print(forest_model(coxph(Surv(time, event) ~ ., tdata), panels))
   ggsave(paste(cohort_name,  feat, event_name, ln_status, ".pdf"), width=plot_width, height=plot_height)
 }
 
-##### usage of the above function for generating forest plots ######@@@@@@@@@@@@@@@
+##### usage of the above function (forest_plot) for generating forest plots ######@@@@@@@@@@@@@@@
 ##the function forest_plot is expecting the event value=1 for an event and anything else will be set to 0 i.e. no event
 
-##params: 
-work_dir = '/Rcode/' ##path where the code and other files are
-cohorts <- list("NOTT","UHCW") ## <"NOTT"|"UHCW">
+work_dir = 'D:/warwick/tasks/Nottingham_Exemplar/ML/cellular_analysis/R' ##path where the code and other files are located
+cohorts <- list("Cohort-B") ## <"Cohort-A"|"Cohort-B">
 
 feature_list <- list("BRACE")
 ln_status_list <- list(0, 1) ##lymph node status. 0 for LN-, 1 for LN 0-3
 event_name_list <- list("DMFS","BCSS") ##<"DMFS"|"BCSS">
 censor_at = 120 ##months at which to censor the data (time, event)
-plot_width = 12 ##width of the plot when saving
+plot_width = 10 ##width of the plot when saving
 plot_height = 5 ##width of the plot when saving
 
 for (c in cohorts){
-  if (c == "NOTT"){
-    csv_name = "NOTT_combi_valid_magee2_onco.csv"
-  }
-  if (c == "UHCW"){
-    csv_name = 'UHCW_BRACE_test_onco_modified.csv'
-  }
+  ## feature file along with time/event. these files can be generated by python script at /survival_analysis/KM_curves.py
+  ## the files can be put in the current working directory as set above work_dir.
+  
+  csv_name = paste0(c, '_for_forest_plots.csv')
   
   for (feature in feature_list)
     for (event_name in event_name_list)
--- survival_analysis/KM_curves.py
+++ survival_analysis/KM_curves.py
@@ -3,6 +3,7 @@ from lifelines import CoxPHFitter, KaplanMeierFitter
 from lifelines.statistics import logrank_test
 from lifelines.utils import concordance_index
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,6 +11,7 @@ import os
 
 ## script for generating Kaplan Meier (KM) curves for BRACE marker and other clinicopathological variables
 ## note that the Mean C-Indices might differ slightly from the reported results because of the 1000 bootstrap runs
+## this script also generate .csv file which can then be used with the accompanied R script (/Forest_plots/forest_plot_R_script.R) to generate forest plots
 
 def apply_preprocess(df_temp, subset, feats_list, time_col, event_col, censor_at, discov_mean_std_path):
     df_temp = df_temp.fillna(0)
@@ -21,7 +23,7 @@ def apply_preprocess(df_temp, subset, feats_list, time_col, event_col, censor_at
     ## convert all events other than 1 to 0
     df_temp.loc[df_temp[event_col] > 1, event_col] = 0
 
-    ## remove chemotherapy treated cases
+    ## remove chemotherapy cases
     df_temp.drop(df_temp[df_temp['Chemotherapy'] == 1].index, inplace=True)
 
      ##apply lymph node filtering i.e consider lymph node negative, or consider lymph node 0-3
@@ -32,36 +34,61 @@ def apply_preprocess(df_temp, subset, feats_list, time_col, event_col, censor_at
         df_temp.drop(df_temp[df_temp['Endocrine Therapy'] != 1].index, inplace=True)
         df_temp.drop(df_temp[df_temp['Lymph Node status'] == 1].index, inplace=True)
 
-    train_mean_std_df = pd.read_csv(discov_mean_std_path)
+    if discov_mean_std_path != -1:
+        train_mean_std_df = pd.read_csv(discov_mean_std_path)
 
-    val_data_temp = df_temp[feats_list]
-    val_data_temp = (val_data_temp - train_mean_std_df['mean'].values.tolist())/ train_mean_std_df['std'].values.tolist() ##standardise using training mean/std
-    val_data_temp['time'] = df_temp[time_col]
-    val_data_temp['event'] = df_temp[event_col]
+        val_data_temp = df_temp[feats_list]
+        val_data_temp = (val_data_temp - train_mean_std_df['mean'].values.tolist())/ train_mean_std_df['std'].values.tolist() ##standardise using training mean/std
+        val_data_temp['time'] = df_temp[time_col]
+        val_data_temp['event'] = df_temp[event_col]
+    else:
+        val_data_temp = df_temp[feats_list]
+        val_data_temp['time'] = df_temp[time_col]
+        val_data_temp['event'] = df_temp[event_col]
 
     return val_data_temp
 
+##add new features based on some equation e.g Magee etc
+def add_new_features(df):
+    ## New Magee equation2: recurrence score = 18.8042 + (NPI)*2.34123 + (ER H-score)*(-0.03749) + (PR H-score)*(-0.03065) + (0 for negative, 1.82921 for equivocal and 11.51378 for HER2 positive)+tumour_size*0.04267.
+    
+    PR_Hscore = 0 ## PR Hscore not available in the clinical file
+    HER2 = 0 ## all cases are HER2 negative
+
+    df_new = pd.DataFrame()
+    for ind, row in df.iterrows():
+        
+        magee_new2 = 18.8042 + row['NPI']*2.34123 + row['ER Hscore']*(-0.03749) + (PR_Hscore)*(-0.03065) + HER2 + row['Tumour_Size']*0.04267
+        row['Magee_new2'] = magee_new2
+
+        magee_new2_no_npi = 18.8042 + row['ER Hscore']*(-0.03749) + (PR_Hscore)*(-0.03065) + HER2 + row['Tumour_Size']*0.04267
+        row['Magee_new2_no_npi'] = magee_new2_no_npi
+
+        df_new = df_new.append(row, ignore_index=True)
+    
+    return df_new
+
 def generate_KM_curves(path_to_model_a, path_to_model_b, features_df, cutoff_value, path_to_save_results, event_name, model_name, LN_status):
-    ## load the Cox PH model that was fitted on the discovery set of Cohort-A (Nottingham)
+    ## load the Cox PH model that was fitted on the discovery set of Cohort-A
     with open(path_to_model_a, 'rb') as file:
         fitted_model = pickle.load(file)
     
-    BRACE_score_val = fitted_model.predict_partial_hazard(features_df)
+    ## make predictions on the test set
+    score_val = fitted_model.predict_partial_hazard(features_df)
     
-    ## print the p-value, HR, and Confidence Interval
-    # Use mean value in the discovery set as the cut-off value and divide subjects int the validation set into two groups
-    upper = BRACE_score_val >= cutoff_value
+    # Use the cut off value selected based on the discovery set to stratify subjects in the validation set into high and low risk groups
+    upper = score_val >= cutoff_value
     T_upper_test = features_df['time'][upper]
     E_upper_test = features_df['event'][upper]
-    lower = BRACE_score_val < cutoff_value
+    lower = score_val < cutoff_value
     T_lower_test = features_df['time'][lower]
     E_lower_test = features_df['event'][lower]
 
-    # Log-rank test: if there is any significant difference between the groups being compared
+    # Log-rank test: check if there is any significant difference between the groups being compared
     results = logrank_test(T_lower_test, T_upper_test, E_lower_test, E_upper_test)
     val_pvalue = round(results.p_value, 4)
 
-    features_df[model_name] = BRACE_score_val
+    features_df[model_name] = score_val
     model_score_df = features_df[[model_name, 'time', 'event']]
 
     with open(path_to_model_b, 'rb') as file:
@@ -69,6 +96,7 @@ def generate_KM_curves(path_to_model_a, path_to_model_b, features_df, cutoff_val
 
     val_cindex = fitted_model.score(features_df, scoring_method="concordance_index")
     
+    ## compute the hazard ratios and confidence intervals for the test set
     cph = CoxPHFitter(baseline_estimation_method='breslow', l1_ratio=0.5, penalizer=0.001).fit(model_score_df, 'time', 'event')
     val_hzratio = round(cph.hazard_ratios_[model_name], 2)
 
@@ -82,7 +110,7 @@ def generate_KM_curves(path_to_model_a, path_to_model_b, features_df, cutoff_val
     except OverflowError:
         vhz_ci_high = float('inf')
 
-    ## bootstraps
+    ## 1000 bootstraps on the test set to compute mean +- standard deviation for the C-Index
     c_indices_val = []
 
     rng = np.random.RandomState()
@@ -126,8 +154,8 @@ def generate_KM_curves(path_to_model_a, path_to_model_b, features_df, cutoff_val
     elif event_name == 'Survival Status':
         event_type = 'BCSS'
 
-    ax = km_upper.fit(T_upper_test, event_observed=E_upper_test, label='high').plot_survival_function(ax=ax, show_censors=True, censor_styles={'ms': 5}, color='r', ci_show=False, xlabel='Months', ylabel= event_type + ' Probability')
-    ax = km_lower.fit(T_lower_test, event_observed=E_lower_test, label='low').plot_survival_function(ax=ax, show_censors=True, censor_styles={'ms': 5}, color='b', ci_show=False, xlabel='Months', ylabel= event_type + ' Probability')
+    ax = km_upper.fit(T_upper_test, event_observed=E_upper_test, label=model_name+'-high').plot_survival_function(ax=ax, show_censors=True, censor_styles={'ms': 5}, color='r', ci_show=False, xlabel='Months', ylabel= event_type + ' Probability')
+    ax = km_lower.fit(T_lower_test, event_observed=E_lower_test, label=model_name+'-low').plot_survival_function(ax=ax, show_censors=True, censor_styles={'ms': 5}, color='b', ci_show=False, xlabel='Months', ylabel= event_type + ' Probability')
     from lifelines.plotting import add_at_risk_counts
     add_at_risk_counts(km_upper, km_lower, ax=ax, fig=fig, fontsize=int(font_size*1) )
     ax.get_legend().remove()
@@ -135,57 +163,99 @@ def generate_KM_curves(path_to_model_a, path_to_model_b, features_df, cutoff_val
     plt.subplots_adjust(left=0.2)
     plt.savefig(path_to_save_results + model_name + '_' + event_type + '_' + LN_status + '.pdf', format='pdf', dpi=600)
 
-    return val_pvalue, cindex_mean, cindex_std, val_hzratio, vhz_ci_low, vhz_ci_high
+    return val_pvalue, cindex_mean, cindex_std, val_hzratio, vhz_ci_low, vhz_ci_high, score_val
 
 if __name__ == '__main__':
+    print('Generating survival results...')
     
-    ## path to a csv file containing the features along with the time_to_event and event
-    feature_df = pd.read_csv('./features/feats_val.csv')
-
-    ## path where the KM plots should be saved
-    plot_save_path =  './results/'
-
+    ## in the provided clinical files the time and event columns are as follows
     event_of_interest = {'DMFS': {'time': 'TTDM/ month', 'event': 'Distant Metastasis'}, 'BCSS': {'time': 'Breast cancer specific survival/ month', 'event': 'Survival Status'}}
     censor_months = 120 #in months. e.g 10 years = 120 months. 180 for 15 years, 240 for 20 years. Use -1 if no censoring is required 
-
-    results_df = pd.DataFrame()  ## df to save the results
-
-    if not os.path.isdir('./results'):
-        os.mkdir('./results')
-
-    for event_type in ['DMFS', 'BCSS']: ## for each event type i.e. Distant Metastatsis Free Survival and Breast Cancer Specific Survival
-        for subset_name in ['LN-', 'LN+']: ## LN- means no postive lymph nodes, LN+ means 0 to 3 postives lymph nodes
-            for model_name in ['Grade', 'NPI', 'BRACE']: ## three types of prognostic markers. the first two (Grade and NPI) are based on clinicopathological variables whereas the last (BRACE) is based on ML features
-                ## path to CoxPH model fitted on initial features. The score from this model_a is then used as a new variable to fit model_b below.
-                path_to_saved_model_a = './models/fitted_' + model_name + '_' + event_type + '_cox_model_a.pkl'
-
-                ## path to CoxPH model fitted on the score obtained from model_a above
-                path_to_saved_model_b = './models/fitted_' + model_name + '_' + event_type + '_cox_model_b.pkl'
-
-                ## path to the discovery statistics used to standardise the data
-                discovery_mean = './models/' + model_name + '_' + event_type + '_discov_mean_std.csv'
-
-                if model_name == 'BRACE':
-                    ## list of ML features included in BRACE marker
-                    feats_list = ['per_g1','per_g2','per_g3','tumor_pixels_per','inter_WSI_S_density_LL_cooccur','pcontrast','stromal_contrast','imitos_70p_score_thresh1_8_thresh2_20_mj']
-                    cutoff_value = 1.28
-                elif model_name == 'NPI':
-                    feats_list = ['NPI']
-                    cutoff_value = 1.11
-                elif model_name == 'Grade':
-                    feats_list = ['Grade']
-                    cutoff_value = 2
-                
-                time_column = event_of_interest[event_type]['time']
-                event_column = event_of_interest[event_type]['event']
-                
-                df = apply_preprocess(feature_df, subset_name, feats_list, time_column, event_column, censor_months, discovery_mean)
-
-                pvalue, cindex, cindex_std, hzratio, hz_ci_low, hz_ci_high = generate_KM_curves(path_to_saved_model_a, path_to_saved_model_b, df, cutoff_value, plot_save_path, event_column, model_name, subset_name)
-                results_df = pd.concat([results_df, pd.DataFrame([{'Event': event_type, 'LN status':subset_name, 'n': df.shape[0], 'Model': model_name, 
-                                                                  'Mean_C-Index': cindex, 'C-Index_Std': cindex_std, 'p-value': pvalue, 
-                                                                  'HR': hzratio, 'CI-low': hz_ci_low, 'CI-high': hz_ci_high}])], ignore_index=True)
     
-    results_df.to_csv('./results.csv')
-    print(results_df)
+    for cohort in ['Cohort-A','Cohort-B']: ## ['Cohort-A', 'Cohort-B]
+        print('Cohort: ', cohort)
+        results_df = pd.DataFrame()  ## Dataframe to save the results
+
+        ## path to a csv file containing the features along with the time_to_event and event
+        ## the provided clinical file is for the external cohort (ie. Cohort-B in the manuscript)
+        
+        feature_df = pd.read_csv('./features/' + cohort + '.csv')
+
+        ## path where the KM plots should be saved
+        results_main_path =  './results/'
+        results_save_path = results_main_path + cohort + '/'
+        
+        if not os.path.isdir(results_main_path):
+            os.mkdir(results_main_path)
+        
+        if not os.path.isdir(results_save_path):
+            os.mkdir(results_save_path)
+
+        for event_type in ['DMFS', 'BCSS']: ## for each event type i.e. Distant Metastatsis Free Survival and Breast Cancer Specific Survival
+            print('Event type: ', event_type)
+            for subset_name in ['LN-', 'LN+']: ## LN- means no postive lymph nodes, LN+ means 0 to 3 postives lymph nodes
+                for model_name in ['Grade', 'NPI', 'BRACE']: ## three types of prognostic markers. the first two (Grade and NPI) are based on clinicopathological variables whereas the last (BRACE) is based on ML features
+                    ## path to CoxPH model fitted on initial features. The score from this model_a is then used as a new variable to fit model_b below.
+                    path_to_saved_model_a = './models/fitted_' + model_name + '_cox_model_a.pkl'
+
+                    ## path to CoxPH model fitted on the score obtained from model_a above
+                    path_to_saved_model_b = './models/fitted_' + model_name + '_cox_model_b.pkl'
+
+                    ## path to the discovery statistics used to standardise the data
+                    if model_name == 'BRACE':
+                        discovery_mean = './models/' + model_name + '_discov_mean_std.csv'
+                    else:
+                        discovery_mean = -1
+
+                    if model_name == 'BRACE':
+                        ## list of ML features included in BRACE marker
+                        feats_list = ['per_g1','per_g2','per_g3','tumor_pixels_per','inter_WSI_S_density_LL_cooccur','pcontrast','stromal_contrast','imitos_70p_score_thresh1_8_thresh2_20_mj']
+                        cutoff_value = 1.28
+                    elif model_name == 'NPI':
+                        feats_list = ['NPI']
+                        cutoff_value = 0.94
+                    elif model_name == 'Grade':
+                        feats_list = ['Grade']
+                        cutoff_value = 2 #2
+                    
+                    time_column = event_of_interest[event_type]['time']
+                    event_column = event_of_interest[event_type]['event']
+                    
+                    df = apply_preprocess(feature_df, subset_name, feats_list, time_column, event_column, censor_months, discovery_mean)
+
+                    pvalue, cindex, cindex_std, hzratio, hz_ci_low, hz_ci_high, score = generate_KM_curves(path_to_saved_model_a, path_to_saved_model_b, df, cutoff_value, results_save_path, event_column, model_name, subset_name)
+                    results_df = pd.concat([results_df, pd.DataFrame([{'Event': event_type, 'LN status':subset_name, 'n': df.shape[0], 'Model': model_name, 
+                                                                    'Mean_C-Index': cindex, 'C-Index_Std': cindex_std, 'p-value': pvalue, 
+                                                                    'HR': hzratio, 'CI-low': hz_ci_low, 'CI-high': hz_ci_high}])], ignore_index=True)
+                    
+                    ## save the features for Forest plots generation in R script (/Forest_plots/forest_plot_R_script.R)
+                    if model_name == 'BRACE' and event_type == 'DMFS' and subset_name == 'LN+':
+                        
+                        df_new = df.copy()
+                        df_new['BRACE'] = score
+                        df_new.drop(feats_list, axis=1, inplace=True)
+                        df_new.drop(['time', 'event'], axis=1, inplace=True)                              
+                        
+                        ## add the required columns
+                        ## rename some of the column causing issues in R script
+                        for c, cn in zip(['Invasive Tumour Size (cm)', 'Associated LCIS', 'Associated DCIS'], ['Tumour_Size', 'Associated_LCIS', 'Associated_DCIS']):
+                            df_new[cn] = feature_df[c]
+
+                        for c in ['Grade', 'NPI', 'Lymph Node status', 'Multifocality', 'Age at Diagnosis', 'ER Hscore', 'PR Hscore', 'LVI',
+                                  'Survival Status', 'Breast cancer specific survival/ month', 'Distant Metastasis', 'TTDM/ month']:
+                            df_new[c] = feature_df[c]
+                        
+                        ## change grade 1,2 to 1 and grade 3 to 2
+                        df_new.loc[df_new['Grade'] < 3, 'Grade'] = 1
+                        df_new.loc[df_new['Grade'] > 2, 'Grade'] = 2
+                        
+                        if cohort == 'Cohort-A':
+                            df_new = add_new_features(df_new)
+
+                        ## this csv file is used as an input to the R script under /Forest_plots/forest_plot_R_script.R to generate the forest plots
+                        df_new.to_csv('./features/' +  cohort + '_for_forest_plots.csv')
+        
+        results_df.to_csv(results_save_path + 'results.csv')
+        print(results_df)
+        
     print('End')
